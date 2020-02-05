ffc4dfec4195ff5246bf6011fc7bccfbe87825fc
Charles Loehle <clguitar1@gmail.com>
2020-02-05 13:52:03 -0500

    more prep

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
 const PORT = process.env.PORT || 5000;
 
