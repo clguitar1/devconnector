e48d25e248bb3d3222f4bc7d1c977cf6d4111c7e
Charles Loehle <clguitar1@gmail.com>
2020-02-07 07:21:36 -0500

    fixed error in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api/users', require('./routes/api/users.routes'));
 
 // setve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(expres.static('client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
